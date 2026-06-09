172f36cb9aab50298bb81f6d8a9310994601f96f
SUBHADEEP BISWAS <147643078+subhadeep104647@users.noreply.github.com>
2026-06-09 20:37:42 +0530

    done

--- naruto/src/Pages/home.jsx
+++ naruto/src/Pages/home.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
-    <div className='bg-amber- flex flex-row w-full mt-8 items-center justify-between gap-5'>
+    <div className='flex flex-col'>
+      <div className='bg-amber- flex flex-row w-full mt-8 items-center justify-between gap-5'>
 
         <div className=' flex flex-col w-1/2 gap-10'>
           <div><h1 className=' text-gray-200 tracking-widest text-sm font-semibold border-2 border-gray-600 w-50 flex items-center justify-center py-1 rounded-l-full rounded-r-full bg-white/5 backdrop-blur-2xl'>AKATSUKI LEADER</h1></div>
@@ -14,6 +16,10 @@ const Home = () => {
         <img src="/PainLogo.svg" alt="My Pic" className='h-150 ml-22'/>
         </div>
 
+      </div>
+      <div className='flex flex-row items-center justify-center'>
+        <div><button className='w-50 h-50 border-2 border-gray-400/50 rounded-2xl bg-fuchsia-700'><Link to="/Pain"><img src='/PainLogo.svg'/></Link></button></div>
+      </div>
     </div>
   )
 }
